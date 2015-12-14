1ede7f7f36b9c147a54475427c3d2df774b66dc6
Daniil Tarakanov <daniil@tarakanov.me>
2015-12-14 22:37:03 +0000

    NLP upgrade

--- src/nlp/src/nlp_lib/nlpNode.py
+++ src/nlp/src/nlp_lib/nlpNode.py
@@ -2,7 +2,7 @@
 
 import rospy
 from StringIO import StringIO
-import pycurl, urllib, json, pprint
+import pycurl, urllib, json, pprint, time, datetime
 from nlp.msg import SpcCmd
 from nlp.msg import SpcNLP
 from nlp.msg import NLPRes
@@ -11,6 +11,7 @@ from std_msgs.msg import String
 CONFIDENCE_VALUE = 0.5
 
 class _Constants:
+	questionDict = {'How are you?': 'state', 'What is your name?': 'name', 'How old are you?': 'age', 'How many hours did you sleep last night?': 'sleep', 'What is the last thing you ate, and when?': 'ate', 'What is the last thing you drunk, and when?': 'drunk', 'Are you currently pregnant?': 'yn', 'Do you have a sickle cell disease?': 'yn', 'Are you wearing any metal jewelry?': 'yn', 'Do you have any loose teeth, caps or crowns?': 'yn', 'Are you wearing glasses or contact lenses?': 'yn', 'Did pee recently?': 'yn', 'Do you have any prosthesis?': 'yn', 'Did you ever have any exposure to the mad cow disease?': 'yn', 'How often do find it hard to wind down?': 'frequency', 'How often aware of dryness in your mouth?': 'frequency', 'How often do you find that you cannot seem to experience any positive feeling at all?': 'frequency', 'How do you rate the level of service?': 'quality'};
     SpcNLP_topic = "SPC_2_NLP"
     NLPCnc_topic = "NLP_2_CNC"
     CncSpc_topic = "CNC_2_SPC"
@@ -47,16 +48,24 @@ class NLP:
 			answer = self.getAnswerWIT(msg.response)
 			readyAnswer = self.prepareAnswer(answer)
 			rospy.logwarn(readyAnswer)
+
+			expected = questionDict[self.question]
+
+			if(readyAnswer == 'error' || not readyAnswer):
+				if self.sys_state == 21:
+					response.res_type = 1
+				else:
+					response.res_type = 2
+			elif(readyAnswer[0] == 'ate' && (expected == 'ate' || expected == 'drunk')):
+				
+			elif(readyAnswer[0] == expected || ((readyAnswer[0] == 'yes' || readyAnswer[0] == 'no') && expected == 'yn'))
+				response.response = readyAnswer[0]
+				response.res_type = 0
+
+
 			readyAnswerJSON = json.encode(readyAnswer)
-			
 			response.response = readyAnswerJSON
 			response.res_type = 0
-
-			# if readyAnswer[0] == 'string':
-			# 	response.response = readyAnswer
-			# 	response.res_type = 0
-			# elif readyAnswer[0] == 'ate':
-			# 	response = AteRes()
 
 
 		rospy.logwarn("NLP sending response: %s  type: %d\n", response.response, response.res_type)
@@ -103,6 +112,9 @@ class NLP:
 			if 'age_of_person' in entities:
 				record.append('age')
 				record.append(entities['age_of_person'][0]['value'])
+			elif 'name' in entities:
+				record.append('name')
+				record.append(entities['age_of_person'][0]['value'])
 		elif(intent == 'ate'):
 			record.append('ate')
 			record.append([])
@@ -116,10 +128,27 @@ class NLP:
 					record[2].append(item['value'])
 			if 'datetime' in entities:
 				for item in entities['datetime']:
-					record[3].append(item['value'])
-		elif(intent == 'yes')
+					time1 = datetime.datetime.strptime(item['value'], "%d %B %Y at %H:%M:%S %Z").timetuple()
+					record[3].append(time1)
+		elif(intent == 'yes'):
 			record.append('yes')
-		elif(intent == 'deny')
+		elif(intent == 'deny'):
 			record.append('no')
+		elif(intent == 'sleep'):
+			record.append('sleep')
+			datetime = []
+			if 'duration' in entities:
+				record.append(entities['duration'][0]['value'])
+			elif 'datetime' in entities:
+				for item in entities['datetime']:
+					datetime.append(item['value'])
+			if datetime.count == 2:
+				# Calc the difference
+				convTime1 = datetime.datetime.strptime(datetime[0], "%d %B %Y at %H:%M:%S %Z").timetuple()
+				convTime2 = datetime.datetime.strptime(datetime[1], "%d %B %Y at %H:%M:%S %Z").timetuple()
+				difference = abs((time.mktime(convTime1) - time.mktime(convTime2)) / 216000)
+				record.append(difference)
+			else:
+				return 'error'
 
 		return record
